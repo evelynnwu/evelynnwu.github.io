4a82dc7af217db27728bfc2b87419cb6589a9d89
Evelyn Wu <evelynwu@andrew.cmu.edu>
2025-06-17 09:48:42 -0700

    configured tailwind

--- script.js
+++
@@ -1,9 +0,0 @@
-const popup = document.getElementById("popup");
-const emailBtn = document.getElementById("email-btn");
-const closeBtn = document.getElementById("close-popup");
-
-emailBtn.onclick = () => popup.classList.remove("hidden");
-closeBtn.onclick = () => popup.classList.add("hidden");
-popup.onclick = (e) => {
-  if (e.target === popup) popup.classList.add("hidden");
-};

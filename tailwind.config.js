ba56d77e1116855d093af43a22c2d4a916e72e0d
Evelyn Wu <evelynwu@andrew.cmu.edu>
2025-06-17 10:37:51 -0700

    migrated to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./*.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "theme-blue": "rgb(94, 131, 252)",
+        "theme-lb": "rgb(150, 173, 250)",
+        "theme-purple": "rgb(162, 151, 255)",
+      },
+      fontFamily: {
+        header: ["Ubuntu", "sans serif"],
+        normal: ["Roboto Mono", "serif"],
+      },
+    },
   },
   plugins: [],
 };
